98dc7da610af94000870a270bebfb4e680d8d2eb
Elena <gudulina@mail.ru>
2022-05-01 12:07:32 +0700

    Решение задания 2

--- Second task/Program.cs
+++ Second task/Program.cs
@@ -2,3 +2,21 @@
 // 1 -> 1
 // 2 -> 0.5
 // 0.25 -> 4
+
+double a = 1;
+Console.Write (a);
+Console.Write (" -> ");
+a = 1 / a;
+Console.WriteLine (a);
+
+double b = 2;
+Console.Write (b);
+Console.Write (" -> ");
+b = 1 / b;
+Console.WriteLine (b);
+
+double c = 0.25;
+Console.Write (c);
+Console.Write (" -> ");
+c = 1 / c;
+Console.WriteLine (c);
