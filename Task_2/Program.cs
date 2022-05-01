b9c91b530f482f637ff11f0ea4397a6d3ea51aa4
Elena <gudulina@mail.ru>
2022-05-01 13:44:03 +0700

    Решение задачи 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = int.Parse (Console.ReadLine ());
+int b = int.Parse (Console.ReadLine ());
+int c = int.Parse (Console.ReadLine ());
+
+int max = a;
+int min = b;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+if (a < min)
+{
+    min = a;
+}
+
+if (c < min)
+{
+    min = c;
+}
+Console.WriteLine ($"max = {max}, min = {min}");
